86b090bf0ed7efab9dbfa97dc1cce6a504cce95b
Charlie Billadeau <charlie.billadeau@gmail.com>
2020-08-06 17:56:01 -0500

    Updates

---
+++ precache-manifest.08b31565ecb941e9d000c9c03db498fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ff444d3650e72fe4880fa61d239642",
+    "url": "/dota-api/index.html"
+  },
+  {
+    "revision": "543e0de6a3edecd0ae85",
+    "url": "/dota-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2725ea56b41684b386d4",
+    "url": "/dota-api/static/css/main.511a6aba.chunk.css"
+  },
+  {
+    "revision": "543e0de6a3edecd0ae85",
+    "url": "/dota-api/static/js/2.2419ed65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dota-api/static/js/2.2419ed65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2725ea56b41684b386d4",
+    "url": "/dota-api/static/js/main.f621e528.chunk.js"
+  },
+  {
+    "revision": "42f25dca5e4d4f626687",
+    "url": "/dota-api/static/js/runtime-main.718aae4d.js"
+  },
+  {
+    "revision": "92cd0203c7ae30e5d5b8261bc8b16deb",
+    "url": "/dota-api/static/media/dotalogo2.92cd0203.png"
+  },
+  {
+    "revision": "181849d985d2519d0502f8624cce53e9",
+    "url": "/dota-api/static/media/spinner.181849d9.gif"
+  }
+]);
