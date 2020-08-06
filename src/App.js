130461a4ed4b499840828428699c55881232c09c
Charlie Billadeau <charlie.billadeau@gmail.com>
2020-08-06 17:59:03 -0500

    updating routes for gh pages

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ const App = () => {
       <Switch>
         <Route
           exact
-          path='/'
+          path='/dota-api'
           render={() => (
             <>
               <Search setSearch={setSearch} search={search} />
@@ -82,7 +82,7 @@ const App = () => {
           )}
         />
         <Route
-          path='/:heroName'
+          path='/dota-api/:heroName'
           render={() => <HeroPage filteredHeroes={filteredHeroes} />}
         />
         <Route component={Error404} />
